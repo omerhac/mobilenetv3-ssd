f373850f452257e7591522e6d1862b273ada48c6
omerhac <omerhac94@gmail.com>
2021-05-30 19:54:05 +0300

    added annotations downloading

--- download_model_and_data.py
+++ download_model_and_data.py
@@ -14,7 +14,7 @@ def download_model(model_url, models_dir, filename):
     print('Finished downloading!')
 
 
-def download_dataset(dataset_url, datasets_dir, dataset_name):
+def download_dataset_images(dataset_url, datasets_dir, dataset_name):
     """Download dataset from dataset url and unzip"""
     if not os.path.exists(datasets_dir):
         os.mkdir(datasets_dir)
@@ -22,6 +22,7 @@ def download_dataset(dataset_url, datasets_dir, dataset_name):
     request.urlretrieve(dataset_url,
                         filename=f'{datasets_dir}/{dataset_name}.zip')
     print('Finished downloading!')
+
     # unzip dataset
     print('Extracting ....')
     with zipfile.ZipFile(f'{datasets_dir}/{dataset_name}.zip', 'r') as zip:
@@ -30,13 +31,32 @@ def download_dataset(dataset_url, datasets_dir, dataset_name):
     print('Finished extracting.')
 
 
+def download_dataset_annotations(dataset_url, datasets_dir):
+    """Download dataset annotations from dataset url and unzip"""
+    if not os.path.exists(f'{datasets_dir}/annotations'):
+        os.mkdir(f'{datasets_dir}/annotations')
+    print(f'Downloading coco dataset annotations into {datasets_dir}/annotations ..... (It might take some time)')
+    request.urlretrieve(dataset_url,
+                        filename=f'{datasets_dir}/annotations.zip')
+    print('Finished downloading!')
+
+    # unzip annotations
+    print('Extracting ....')
+    with zipfile.ZipFile(f'{datasets_dir}/annotations.zip', 'r') as zip:
+        zip.extractall(f'{datasets_dir}')
+    os.remove(f'{datasets_dir}/annotations.zip')
+    print('Finished extracting.')
+
+
 if __name__ == '__main__':
     model_url = 'https://github.com/lufficc/SSD/releases/download/1.2/mobilenet_v3_ssd320_voc0712.pth'
     datasets_url = 'http://images.cocodataset.org/zips/val2017.zip'
+    annotations_url = 'http://images.cocodataset.org/annotations/annotations_trainval2017.zip'
 
     # download model
     download_model(model_url, 'models', 'mobilenetv3_ssd.model')
     # download dataset
-    download_dataset(datasets_url, 'datasets', 'val2017')
+    download_dataset_images(datasets_url, 'datasets', 'val2017')
+    download_dataset_annotations(annotations_url, 'datasets')
 
 
